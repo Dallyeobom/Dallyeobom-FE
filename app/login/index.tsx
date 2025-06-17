ecbdea3241ee97786d5c92913307399b043e7490
yunajoe <yunajoe@gmail.com>
2025-06-17 23:18:14 +0900

    style: 첫 화면 이미지 layout

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'expo-router';
 import React, { useEffect } from 'react';
-import { StyleSheet, useWindowDimensions, View } from 'react-native';
+import { Image, StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import Animated, {
   Easing,
   useAnimatedStyle,
@@ -35,18 +35,32 @@ const LoginScreen: React.FC = () => {
 
   return (
     <View style={[styles.container, { paddingTop: insets.top }]}>
-      <Animated.View style={[styles.sliderContainer, animatedStyle]}>
-        <Animated.Image
-          source={require('@/assets/images/background.png')}
-          style={styles.image}
-          resizeMode="cover"
-        />
-        <Animated.Image
-          source={require('@/assets/images/background-2.png')}
-          style={styles.image}
-          resizeMode="cover"
-        />
-      </Animated.View>
+      <View style={styles.subContainer}>
+        <View style={styles.mainTextContainer}>
+          <Text style={styles.mainText}>당신의 런닝 파트너</Text>
+          {/* <View style={{ width: 200, height: 50 }}>
+            <Image source={require('@/assets/images/typo-logo.png')}></Image>
+          </View> */}
+          <Text style={styles.mainText}>
+            가볍게 뛰고 싶을 때, 지금 바로 시작해보세요!
+          </Text>
+        </View>
+        <Animated.View style={[styles.sliderContainer, animatedStyle]}>
+          <Animated.Image
+            source={require('@/assets/images/background.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <Animated.Image
+            source={require('@/assets/images/background-2.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </Animated.View>
+        <View style={styles.button}>
+          <Image source={require('@/assets/images/kakao.png')}></Image>
+        </View>
+      </View>
     </View>
   );
 };
@@ -55,6 +69,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     overflow: 'hidden',
+  },
+  subContainer: {
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    flex: 1,
   },
   sliderContainer: {
     flexDirection: 'row',
@@ -65,6 +85,26 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     flex: 1,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 40,
+    left: '25%',
+    transform: [{ translateX: -50 }],
+    zIndex: 10,
+  },
+  mainTextContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '30%',
+    transform: [{ translateX: -50 }, { translateY: -50 }],
+    zIndex: 10,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  mainText: {
+    color: '#fff',
+    fontWeight: '700',
   },
 });
 
