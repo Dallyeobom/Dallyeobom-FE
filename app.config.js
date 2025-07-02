077716536c96f052d7713cfd203c519028ca5bf2
yunajoe <yunajoe@gmail.com>
2025-07-02 19:51:31 +0900

    feat: kakao  native app key app.json에 넣고, expo Config constant를 사용

--- app.config.js
+++ app.config.js
@@ -1,65 +1,68 @@
 export default {
   expo: {
-    name: "dallyeobom-app",
-    slug: "dallyeobom-app",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "dallyeobomapp",
-    userInterfaceStyle: "automatic",
+    name: 'dallyeobom-app',
+    slug: 'dallyeobom-app',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'dallyeobomapp',
+    userInterfaceStyle: 'automatic',
     newArchEnabled: true,
     ios: {
       supportsTablet: true,
       config: {
-        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
-      }
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      },
     },
     android: {
       config: {
         googleMaps: {
-          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
-        }
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        },
       },
       adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff"
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff',
       },
       edgeToEdgeEnabled: true,
-      package: "com.dallyeobom.dallyeobomapp"
+      package: 'com.dallyeobom.dallyeobomapp',
     },
     web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png"
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png',
     },
     plugins: [
-      "expo-router",
+      'expo-router',
       [
-        "expo-splash-screen",
+        'expo-splash-screen',
         {
-          image: "./assets/images/splash-icon.png",
+          image: './assets/images/splash-icon.png',
           imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#ffffff"
-        }
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+        },
       ],
       [
-        "expo-location",
+        'expo-location',
         {
-          locationAlwaysAndWhenInUsePermission: "This app needs access to location when open and in the background."
-        }
-      ]
+          locationAlwaysAndWhenInUsePermission:
+            'This app needs access to location when open and in the background.',
+        },
+      ],
     ],
     experiments: {
-      typedRoutes: true
+      typedRoutes: true,
     },
     extra: {
       router: {},
+
       eas: {
-        projectId: "0f74721d-543d-4940-8189-f06da04a3194"
+        projectId: '0f74721d-543d-4940-8189-f06da04a3194',
       },
-      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
+      googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      kakaoNativeAppKey: process.env.EXPO_PUBLIC_KAKAO_NATIVE_APP_KEY,
     },
-    owner: "dallyeobom"
-  }
+    owner: 'dallyeobom',
+  },
 };
